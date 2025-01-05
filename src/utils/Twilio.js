f4756ca10b21be230b7256dfed0e927f7d936197
AashishYadav99 <ashish.yadav65411@bbdu.ac.in>
2025-01-05 23:22:52 +0530

    twilio update

---
+++ src/utils/Twilio.js
@@ -0,0 +1,34 @@
+const twilio = require("twilio");
+
+const sendMessage = async (mobileNo, message) => {
+  //mobileNo, message, process.env.TWILLIO_SID,  process.env.TWILLIO_AUTH_TOKEN
+  //process.env.TWILIO_FROM_NO
+  // console.log(process.env.TWILIO_SID, " -->", process.env.TWILIO_AUTH_TOKEN);
+
+  // const client = new twilio('AC9deb70214f8e36fea01366d875fecf2c', '738da883871f6b7a36eb383128a06e70');
+
+  // Initialize Twilio client using environment variables
+  const client = new twilio(
+    process.env.TWILIO_SID,
+    process.env.TWILIO_AUTH_TOKEN
+  );
+
+  const messageres = await client.messages
+    .create({
+      body: message,
+      to: mobileNo, // Text this number
+      from: "+12056064060", // From a valid Twilio number
+    })
+    .then((res) => {
+      console.log(("suuccess", res));
+    })
+    .catch((erorr) => {
+      console.log("error", erorr);
+    });
+
+  return messageres;
+};
+
+module.exports = {
+  sendMessage,
+};
